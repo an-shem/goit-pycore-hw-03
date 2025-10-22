eee7d0f205423c4681504387c7d18b7a6f8a624f
Andrey Shemet <andrey.shemet.nik@gmail.com>
2025-10-22 22:30:24 +0200

    add task_1

---
+++ task_1.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def get_days_from_today(date: str) -> str:
+    
+    date_format = "%Y-%m-%d"
+    
+    try:
+        date_datetime = datetime.strptime(date, date_format)
+        current_datetime = datetime.today()
+        difference_days = (current_datetime - date_datetime).days
+        return difference_days 
+    
+    except ValueError:
+        return ("Incorrect date format")
