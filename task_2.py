7ef46c54223b37534437bd718e314870095277fb
Andrey Shemet <andrey.shemet.nik@gmail.com>
2025-10-22 23:23:16 +0200

    Removed unnecessary indents in tasks 1 and 2, and added parameter checking in task 1.

--- task_2.py
+++ task_2.py
@@ -1,20 +1,14 @@
 import random
 
 def get_numbers_ticket(min:int, max:int, quantity:int) -> list:
-      
     for param in (min, max, quantity):
         if type(param) is not int:
             print("All parameters must be integers!")
             return[]
- 
     are_all_parameters_correct = min >= 1 and max <= 1000 and min < max and quantity >= min and quantity <= max
-    
     if not are_all_parameters_correct:
         return []
-    
     res = set()
-    
     while len(res) != quantity:
         res.add(random.randint(min, max))
-    
     return sorted(list(res))
