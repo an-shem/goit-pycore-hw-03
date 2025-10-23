f85a0b4c706d3372a5af946c08b3c4a77f25d74d
Andrey Shemet <andrey.shemet.nik@gmail.com>
2025-10-23 23:49:49 +0200

    Added checks to the task_1and tasks_2

--- task_1.py
+++ task_1.py
@@ -1,13 +1,22 @@
 from datetime import datetime
 
+
 def get_days_from_today(date: str) -> str:
     if not isinstance(date, str):
-        return("Date must be a string.")
+        return "Date must be a string."
     date_format = "%Y-%m-%d"
     try:
         date_datetime = datetime.strptime(date, date_format)
         current_datetime = datetime.today()
         difference_days = (current_datetime - date_datetime).days
-        return difference_days 
+        return difference_days
     except ValueError:
-        return ("Incorrect date format")
+        return "Incorrect date format"
+
+
+print(get_days_from_today("2025-09-30"))
+print(get_days_from_today("2021-02-24"))
+print(get_days_from_today("2027-12-30"))
+print(get_days_from_today("2027.12.30"))
+print(get_days_from_today("2027/12/30"))
+print(get_days_from_today(20250930))
--- task_2.py
+++ task_2.py
@@ -1,14 +1,26 @@
 import random
 
-def get_numbers_ticket(min:int, max:int, quantity:int) -> list:
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     for param in (min, max, quantity):
         if not isinstance(param, int):
             print("All parameters must be integers!")
-            return[]
-    are_all_parameters_correct = min >= 1 and max <= 1000 and min < max and quantity >= min and quantity <= max
+            return []
+    are_all_parameters_correct = (
+        min >= 1 and max <= 1000 and min < max and quantity >= min and quantity <= max
+    )
     if not are_all_parameters_correct:
         return []
     res = set()
     while len(res) != quantity:
         res.add(random.randint(min, max))
     return sorted(list(res))
+
+
+print(get_numbers_ticket(1, 49, 6))
+print(get_numbers_ticket(1, 36, 5))
+print(get_numbers_ticket(1, "36", 5))
+print(get_numbers_ticket("one", 15, 5))
+print(get_numbers_ticket(20, 15, 5))
+print(get_numbers_ticket(1, 1500, 15))
+print(get_numbers_ticket(-20, 15, 5))
